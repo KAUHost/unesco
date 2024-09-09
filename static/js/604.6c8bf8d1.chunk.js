d931e6dfcdce6677e73ada2d8bcdc1d325968912
ArtomkIvanher <161267462+ArtomkIvanher@users.noreply.github.com>
2024-09-09 17:51:47 +0300

    Updates

---
+++ static/js/604.6c8bf8d1.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkunesco=self.webpackChunkunesco||[]).push([[604],{604:e=>{e.exports=JSON.parse('[{"year1":"","title1":"","text1":"","year2":"","title2":"","text2":""},{"year1":"","title1":"","text1":"","year2":"","title2":"","text2":""}]')}}]);
---
+++ static/js/712.3dd8a23e.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkunesco=self.webpackChunkunesco||[]).push([[712],{712:e=>{e.exports=JSON.parse('[{"year1":"","title1":"","text1":"","year2":"","title2":"","text2":""},{"year1":"","title1":"","text1":"","year2":"","title2":"","text2":""}]')}}]);
